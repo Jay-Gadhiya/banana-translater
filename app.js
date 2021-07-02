f296ae8c5864cf071d038ebe0a054234e7a81c97
Jay-Gadhiya <jaygadhiya9988@gmail.com>
2021-07-02 19:40:00 +0530

    func: query

---
+++ app.js
@@ -0,0 +1,11 @@
+var btnTranslate = document.querySelector("#btn-translate");
+
+function clickHandler()
+{
+    console.log("Clicked!");
+}
+
+
+
+btnTranslate.addEventListener("click", clickHandler)
+
