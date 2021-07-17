becaef6c751fb17eced2db19fe79d34a4d91470c
Jay-Gadhiya <jaygadhiya9988@gmail.com>
2021-07-17 11:42:07 +0530

    add

--- app.js
+++ app.js
@@ -1,10 +1,33 @@
-var btnTranslate = document.querySelector("#btn-translate");
-var textInput = document.querySelector("#txt-input");
-var outputText = document.querySelector("#output");
+var btnTranslate = document.querySelector("#translate-button");
+var textInput = document.querySelector("#translate-input");
+var outputText = document.querySelector("#translate-output");
 
-function clickHandler()
-{
-    outputText.innerText = "Translated: "+ textInput.value;
+
+var serverURL = "https://api.funtranslations.com/translate/minion.json";
+
+
+function getTranslationURL(input) {
+    return serverURL + "?" + "text" + input;
+}
+
+
+// function errorHandler(error) {
+//     console.log("error occured", error);
+//     alert("Something wrong with server: try again after some time");
+// }
+
+
+function clickHandler() {
+    var inputText = textInput.value; //taking input value
+
+    //calling server for processing
+    fetch(getTranslationURL(inputText))
+        .then(response => response.json())
+        .then(json => {
+            var translatedText = json.contents.translated;
+            outputText.innerText = translatedText; // Output.   
+        })
+        // .catch(errorHandler)
 };
 
 
