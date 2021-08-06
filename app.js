ddd9adcf41f653ac281b6e1daa9bbc680ff4c908
Jay-Gadhiya <jaygadhiya9988@gmail.com>
2021-08-06 19:44:39 +0530

    update
    this will make last change

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ var serverURL = "https://api.funtranslations.com/translate/groot.json";
 
 
 function getTranslationURL(input) {
-    return serverURL + "?" + "text" + input;
+    return `${serverURL}?text=${input}`;
 }
 
 
